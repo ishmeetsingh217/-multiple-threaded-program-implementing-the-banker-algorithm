e612fbcc45e1b309b03ccf422f3224bcccef6460
Ishmeet Singh <ishmeetsingh217@gmail.com>
2020-07-22 21:46:49 -0400

    Adding checkRequest function
    It is a function that implements request commands

--- banker-algorithm.c
+++ banker-algorithm.c
@@ -6,4 +6,37 @@
 #include <sys/stat.h>
 #include <time.h>
 
-		
+// Define static array
+int maxRes[5][4]; //Max available resources data from file
+// int allocateRes[5][4]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{1,2,3,1}}; //Allocated resources in each Customers
+int allocateRes[5][4];
+
+
+// function that implement request command		
+void checkRequest(int eachValue[], int availRes[])
+{
+	int flag = 1;
+	// check possibility RQ command
+	for (int i = 1; i < 5; i++)
+	{
+		if ((maxRes[eachValue[0]][i - 1] < eachValue[i]) && (eachValue[i] * 2 > availRes[i]))
+		{
+			flag = 0;
+			break;
+		}
+	}
+	if (flag == 1)
+	{
+		// Reset the available value and allocated values
+		for (int i = 1; i < 5; i++)
+		{
+			availRes[i] = availRes[i] - eachValue[i];
+			allocateRes[eachValue[0]][i - 1] = eachValue[i];
+		}
+		printf("Request is satisfied\n");
+	}
+	else
+	{
+		printf("Request is not satisfied\n");
+	}
+}
